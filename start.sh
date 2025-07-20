53e66366ba10faba746788a48a623e2e60ce9dab
NAVEENRAJ9925 <152049444+NAVEENRAJ9925@users.noreply.github.com>
2025-07-20 10:37:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start FastAPI in background
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+# Start Streamlit in the background
+streamlit run app/streamlit_ui.py --server.port 8501 &
 
-# Start Streamlit in foreground
-streamlit run app/streamlit_ui.py --server.port 8501
+# Start FastAPI (Uvicorn)
+uvicorn app.main:app --host 0.0.0.0 --port 8000
